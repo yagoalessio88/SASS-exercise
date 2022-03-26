ed804917f5657322c9d5e0d3877bbc1e00a9885e
yagoalessio88 <yagoalessio88@gmail.com>
2022-03-26 11:23:51 +0100

    main commit

---
+++ js/main.js
@@ -0,0 +1,28 @@
+const menuBtn = document.querySelector('.menu-btn');
+const hamburguer = document.querySelector('.menu-btn__burguer');
+const nav = document.querySelector('.nav');
+const menuNav = document.querySelector('.menu-nav');
+const navItems = document.querySelectorAll('.menu-nav__item');
+
+let showMenu = false;
+
+menuBtn.addEventListener('click', toggleMenu);
+
+
+function toggleMenu(){
+    if(!showMenu){
+        hamburguer.classList.add('open');
+        nav.classList.add('open');
+        menuNav.classList.add('open');
+        navItems.forEach(item => item.classList.add('open'));
+
+        showMenu = true;
+    } else {
+        hamburguer.classList.remove('open');
+        nav.classList.remove('open');
+        menuNav.classList.remove('open');
+        navItems.forEach(item => item.classList.remove('open'));
+
+        showMenu = false;
+    }
+}
